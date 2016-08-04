caa372b269eb45ca0620a3c6c300fd56c2797b18
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-04 18:54:15 +0200

    corrections base de données

--- init_salesforce_schema.sql
+++ init_salesforce_schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS salesforce.contact (
     mobilePhone     TEXT,
     leadsource      TEXT,
     accountid       TEXT,
-    pictureURL__c   TEXT,
-    preference__c   TEXT,
-    size__c         TEXT,
-    loyaltyid__c    TEXT,
-    password__c     TEXT,
-    fbuserid__c     TEXT,
-    gender__c       TEXT,
-	TypeCompte__c	TEXT,
+    eitech__pictureURL__c   TEXT,
+    eitech__preference__c   TEXT,
+    eitech__size__c         TEXT,
+    eitech__loyaltyid__c    TEXT,
+    eitech__password__c     TEXT,
+    eitech__fbuserid__c     TEXT,
+    eitech__gender__c       TEXT,
+	eitech__TypeCompte__c	TEXT,
     createddate     timestamp
   );
 
@@ -39,9 +39,9 @@ CREATE TABLE IF NOT EXISTS salesforce.campaign (
     startdate       DATE,
     enddate         DATE,
     description     TEXT,
-    image__c        TEXT,
-    campaignpage__c TEXT,
-    publishdate__c  DATE,
+    eitech__image__c        TEXT,
+    eitech__campaignpage__c TEXT,
+    eitech__publishdate__c  DATE,
     type            TEXT,
     status          TEXT
   );
@@ -51,9 +51,9 @@ CREATE TABLE IF NOT EXISTS salesforce.product2 (
     id              BIGSERIAL PRIMARY KEY,
     name            TEXT,
     description     TEXT,
-    image__c        TEXT,
-    productpage__c  TEXT,
-    publishdate__c  DATE,
+    eitech__image__c        TEXT,
+    eitech__productpage__c  TEXT,
+    eitech__publishdate__c  DATE,
     family          TEXT
   );
 
@@ -80,7 +80,7 @@ CREATE TABLE salesforce.eitech__coupon__c
 );
   
 
-INSERT INTO salesforce.campaign (id, name, description, image__c, type, status) VALUES
+INSERT INTO salesforce.campaign (id, name, description, eitech__image__c, type, status) VALUES
     (1, '10% of EcoTruffles', 'Twice as much Eco!', 'http://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/truffles.jpg', 'Offer', 'In Progress'),
     (2, '10% off EcoChocolate: Fair Trade and Organic Chocolates', '0% off chocolate that makes you feel as good as they tastes! Fair Trade and Organic chocolates assortment...', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/ritual2.jpg', 'Offer', 'In Progress'),
     (3, 'Buy 2 Get 1 Free: Dandelion Chocolate for Connoisseurs', 'Purists, Foodies and Afficionados: Buy 2 Bars Get 1 Free', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/Purists+Campaign+.png', 'Offer', 'In Progress'),
@@ -89,7 +89,7 @@ INSERT INTO salesforce.campaign (id, name, description, image__c, type, status) 
     (6, 'Free Shipping on Wine & Chocolate Pairings', 'Free Shipping on all wine, champagne, and chocolate pairings.', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/dandelion2.jpg', 'Offer', 'In Progress'),
     (7, 'Nuts about nuts: 30% off mixed chocolate covered nuts', 'Are you nuts for nuts?', 'http://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/nuts.jpg', 'Offer', 'In Progress');
 
-INSERT INTO salesforce.product2 (id, name, description, image__c, family) VALUES
+INSERT INTO salesforce.product2 (id, name, description, eitech__image__c, family) VALUES
     (1, 'Caramelized Almonds', 'Addictive treats from the popular new boutique chocolatier in San Francisco''s Mission District.', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/feve1.jpg', 'Nibs'),
     (2, 'Dandelion Assortment', 'Bring the flavor of San Francisco boutique chocolate into your home, or present as a gift to the foodie in your life.', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/dandelion2.jpg', 'Nibs'),
     (3, 'Dandelion Small Batch', 'Experience the buzz around San Francisco''s newest boutique chocolatier. These beans are slow roasted whole for unparalleled flavor depth.', 'https://s3-us-west-1.amazonaws.com/sfdc-demo/nibs/dandelion1.jpg', 'Nibs'),
