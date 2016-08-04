54e2d261a406091545975070ab65591045d36bd4
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-04 15:48:26 +0200

    corrections

--- server/coupons.js
+++ server/coupons.js
@@ -1,4 +1,5 @@
-var db = require('./pghelper');
+var db = require('./pghelper'),
+    winston = require('winston');
 
 
 /**
@@ -12,28 +13,28 @@ function addItem(req, res, next) {
         coupon = req.body;
     coupon.consommateur = userId;
 
-    console.log('Adding coupon: ' + JSON.stringify(coupon));
+    winston.info('Adding coupon: ' + JSON.stringify(coupon));
 
     getCoupon(coupon).then(function (coupons) {
-      if(coupons.length > 0) {
-          coupon.created = false;
-        res.send(JSON.stringify(coupons[0]));
-      } else {
-          coupon.created = true;
-        db.query('SELECT sfid FROM salesforce.contact where loyaltyid__c = $1', [userId]).then(function(sfid) {
-            db.query('INSERT INTO salesforce.eitech__coupon__c(eitech__campaign__c, eitech__consommateur__c, eitech__consoloyaltyid_del__c) VALUES ($1, $2, $3) ', [coupon.offerId, sfid, userId]).then(function() {
-                res.send(JSON.stringify({coupon}));
-            }).catch(next);
-        }).catch(next);
+        if (coupons.length > 0) {
+            coupons[0].created = false;
+            res.send(JSON.stringify(coupons[0]));
+        } else {
+            coupon.created = true;
+            db.query('SELECT sfid FROM salesforce.contact where loyaltyid__c = $1', [userId]).then(function (sfid) {
+                db.query('INSERT INTO salesforce.eitech__coupon__c(eitech__campaign__c, eitech__consommateur__c, eitech__consoloyaltyid_del__c) VALUES ($1, $2, $3) ', [coupon.offerId, sfid, userId]).then(function () {
+                    res.send(JSON.stringify(coupon));
+                });
+            });
 
-      }
+        }
     }).catch(next);
 
 }
 
 
 function getCoupon(coupon) {
-  return db.query('select eitech__campaign__c as campaign, eitech__consoloyaltyid_del__c as consommateur, eitech__date_de_consommation__c as date, eitech__commercant__c as commercant from salesforce.eitech__coupon__c where  eitech__campaign__c as campaign=$1 and eitech__consommateur__c as  eitech__consoloyaltyid_del__c =$2', [coupon.offerId, coupon.userId]);
+    return db.query('select eitech__campaign__c as campaign, eitech__consoloyaltyid_del__c as consommateur, eitech__date_de_consommation__c as date, eitech__commercant__c as commercant from salesforce.eitech__coupon__c where  eitech__campaign__c as campaign=$1 and eitech__consommateur__c as  eitech__consoloyaltyid_del__c =$2', [coupon.offerId, coupon.userId]);
 }
 
 exports.addItem = addItem;
