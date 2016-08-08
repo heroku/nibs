aa31fc889754eb55596e06099cabada3bc7239ce
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-08 11:34:31 +0200

    wallet

--- server/coupons.js
+++ server/coupons.js
@@ -10,6 +10,29 @@ function findById(id, userId) {
 	return db.query('select id, eitech__campaign__c as campaign, eitech__consommateur__r__eitech__loyaltyid__c as consommateur, eitech__date_de_consommation__c as date, eitech__commercant__r__eitech__loyaltyid__c as commercant, eitech__Secret__c as secret from salesforce.eitech__coupon__c where id = $1 and eitech__consommateur__r__eitech__loyaltyid__c = $2', [id, userId], true);
 }
 
+
+function createCoupon(coupon) {
+	return getCoupon(coupon).then(function (coupons) {
+		winston.info("Coupons: " + JSON.stringify(coupons));
+		if (coupons.length > 0) {
+			var existingCoupon = coupons[0];
+			
+			return coupons[0].id;
+		} else {
+
+			db.query('INSERT INTO salesforce.eitech__coupon__c(eitech__campaign__c, eitech__consommateur__r__eitech__loyaltyid__c, eitech__Secret__c) VALUES ($1, $2, floor(random() * 1E10)) RETURNING id, eitech__campaign__c as campaign, eitech__consommateur__r__eitech__loyaltyid__c as consommateur, eitech__Secret__c as secret', [coupon.offerId,  coupon.consommateur]).then(function (insertedCoupon) {
+				winston.info("Inserted coupon: " + JSON.stringify(insertedCoupon));
+				
+				return insertedCoupon.id;
+				
+			});
+
+
+		}
+	}):
+}
+
+
 /**
  * @param req
  * @param res
@@ -22,25 +45,19 @@ function addItem(req, res, next) {
 
     winston.info('Adding coupon: ' + JSON.stringify(coupon));
 
-    getCoupon(coupon).then(function (coupons) {
-        winston.info("Coupons: " + JSON.stringify(coupons));
-        if (coupons.length > 0) {
-			var existingCoupon = coupons[0];
-
-            res.send(JSON.stringify({id: coupons[0].id}));
-        } else {
-            
-            db.query('INSERT INTO salesforce.eitech__coupon__c(eitech__campaign__c, eitech__consommateur__r__eitech__loyaltyid__c, eitech__Secret__c) VALUES ($1, $2, floor(random() * 1E10)) RETURNING id, eitech__campaign__c as campaign, eitech__consommateur__r__eitech__loyaltyid__c as consommateur, eitech__Secret__c as secret', [coupon.offerId,  userId]).then(function (insertedCoupon) {
-				winston.info("Inserted coupon: " + JSON.stringify(insertedCoupon));
-                res.send(JSON.stringify({id: insertedCoupon.id}));
-            });
-            
-
-        }
-    }).catch(next);
+	createCoupon(coupon).then(function(id) {
+		res.send(JSON.stringify({id: id}));
+	}).catch(next);
 
 }
 
+function getImage(coupon) {
+	var qr = qrCode.qrcode(10, 'M');
+	qr.addData(JSON.stringify({app: 'Heineken', id: coupon.id, secret: coupon.secret}));
+	qr.make();
+	
+	return qr.createImgTag(4).match(/.*src="data:image\/gif;base64,([\w+/=]*)".*/)[1];
+}
 
 function getById(req, res, next) {
 	var id = req.params.id;
@@ -50,11 +67,8 @@ function getById(req, res, next) {
         var text = JSON.stringify(coupon);
 		console.log(text);
 		if(coupon.date == null) {
-			var qr = qrCode.qrcode(10, 'M');
-			qr.addData(JSON.stringify({app: 'Heineken', id: coupon.id, secret: coupon.secret}));
-			qr.make();
-
-			coupon.base64 = qr.createImgTag(4).match(/.*src="data:image\/gif;base64,([^"]*)".*/)[1]; 
+			
+			coupon.base64 = getImage(coupon);
 		}
        
 		return res.send(JSON.stringify(coupon));
