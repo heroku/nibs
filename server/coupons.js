829e3fe92b9b768fadd6023ac09267a5454856a8
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-10 19:31:30 +0200

    scan

--- server/coupons.js
+++ server/coupons.js
@@ -122,7 +122,7 @@ function consume(req, res, next) {
   var coupon = req.body,
     userId = req.externalUserId;
 
-  db.query('UPDATE salesforce.eitech__coupon__c SET eitech__commercant__r__eitech__loyaltyid__c = $3, eitech__date_de_consommation__c = Date.now() WHERE id = $1 AND eitech__Secret__c = $2 AND eitech__date_de_consommation__c IS NULL', [coupon.id, coupon.secret, userId]).then (function (result) {
+  db.query('UPDATE salesforce.eitech__coupon__c SET eitech__commercant__r__eitech__loyaltyid__c = $3, eitech__date_de_consommation__c = now() WHERE id = $1 AND eitech__Secret__c = $2 AND eitech__date_de_consommation__c IS NULL', [coupon.id, coupon.secret, userId]).then (function (result) {
     winston.info(JSON.stringify(result));
     return res.send(200);
   }, function(err) {
