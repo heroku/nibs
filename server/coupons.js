bfca80b5dd859605f297e805ece66e245da0ac44
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-11 19:13:22 +0200

    report

--- server/coupons.js
+++ server/coupons.js
@@ -133,9 +133,23 @@ function consume(req, res, next) {
 }
 
 
+function getReport(req, res, next) {
+  var userId = req.externalUserId;
+
+  db.query('SELECT ca.id, ca.name as name, ca.eitech__image__c as image, count(*) as count FROM salesforce.eitech__coupon__c co, salesforce.campaign ca WHERE  co.eitech__campaign__c = ca.sfid AND co.eitech__commercant__r__eitech__loyaltyid__c = $1 GROUP BY ca.id, ca.name, ca.eitech__image__c, ca.enddate ORDER BY ca.enddate DESC NULLS FIRST', [userId]).then(function(offers) {
+    var total = 0;
+    for (offer in offers) {
+      total += offer.count;
+    }
+    return res.send(JSON.stringify({total: total, offers: offers}));
+  });
+}
+
+
 exports.addItem = addItem;
 exports.getById = getById;
 exports.createCoupon = createCoupon;
 exports.getImage = getImage;
 exports.check = check;
 exports.consume = consume;
+exports.getReport = getReport;
