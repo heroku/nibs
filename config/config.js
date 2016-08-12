8e80b889eaf0e195daa9ee96f617898bcd44dc4d
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-12 17:41:00 +0200

    config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 angular.module('nibs.config', [])
 
-    .constant('SERVER_URL', process.env.SERVER_URL || '@@serverUrl')
+    .constant('SERVER_URL', '@@serverUrl')
 
-    .constant('FB_APP_ID', process.env.FB_APP_ID || '@@fbAppId')
+    .constant('FB_APP_ID', '@@fbAppId')
 
     .constant('STATUS_LABELS', [
 
