f5ff97fd3cabb2796f573e4553d33a3132b3bf0f
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-08-17 11:39:49 +0200

    push notification

--- server/notifications.js
+++ server/notifications.js
@@ -1,15 +1,17 @@
 var request = require('request'),
-  winston = require('winston');
+  winston = require('winston'),
+  config = require('./config');
 
 var tokens = [];
 
 function sendNotifications() {
   winston.info("sending to " + tokens.toString());
+  winston.info("using api token ", config.ionicApiToken);
   var options = {
     uri: 'https://api.ionic.io/push/notifications',
     method: 'POST',
     headers: [
-      {name: 'Authorization', value: 'Bearer ' + process.env.IONIC_API_TOKEN }
+      {name: 'Authorization', value: 'Bearer ' + config.ionicApiToken }
     ],
     json: {
       tokens: tokens,
@@ -23,7 +25,7 @@ function sendNotifications() {
   function callback(error, response, body) {
     if (!error ) {
       winston.info("Response status:", response.statusCode);
-      winston.info("body: " + body);
+      winston.info("body: " + JSON.stringify(body));
     }
   }
 
