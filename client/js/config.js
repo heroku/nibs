b95a1546b163c7f14d5ab0d984971bee238000ac
tommasofe85 <38831845+tommasofe85@users.noreply.github.com>
2018-05-28 17:02:15 +0200

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -11,7 +11,7 @@ angular.module('nibs.config', [])
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Forastero (For-ah-stare-oh)  is a common base to all but the finest of chocolates.  Our newest Nibs members move as fast as Forastero pods grow.',
-        'Trinitario (Trin-it-air-ee-yo)  is a hybrid combining the superior taste of the criollo bean with the resilience of the forastero bean. It’s not quite as rare as Criollo, but it’s close!',
-        'Criollo (Kree-oh-yo) is the most valued and rare type of cacao. Our highest level of Nibs members share the taste and sophistication of  select Crillolo pods.'
+        'Filo (Beginner) sei una shopper saltuaria, coraggio che puoi avere super possibilità! ',
+        'Matassa (Intermediate) sei ad un ottimo livello di shopper, una esperta nell acquisto, ti mancano solo i superpoteri ',
+        'Gomitolo (Super Power) semplicemente perfetto! '
     ]);
